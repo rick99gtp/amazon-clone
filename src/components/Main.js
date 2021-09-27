3533ff3e9c79beaf36ea30b60bd7f3b45aba6832
Rick Beyer <beyer.richard73@gmail.com>
2021-09-27 09:30:25 -0500

    Main image with mask-image css added.

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <div className='main'>
+            <img className='main__image' src='https://m.media-amazon.com/images/I/61Q+C2yYwyL._SX3000_.jpg' alt='' />
+        </div>
+    )
+}
+
+export default Main
