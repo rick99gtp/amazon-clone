3ace4203c1b8ac5a32c1169fd438256f13eaea0c
Rick Beyer <beyer.richard73@gmail.com>
2021-10-02 13:53:56 -0500

    Fixed issue with rating not showing up properly in checkout

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -3,7 +3,6 @@ import Product from './Product';
 
 const CartItems = () => {
     const cart = useSelector(state => state);
-    console.log(cart);
 
     // map through store.items for all items in cart
     const items = cart.items.map(function (item) {
@@ -12,7 +11,7 @@ const CartItems = () => {
                 key={item.id}
                 id={item.id}
                 title={item.title}
-                rating={item.stars}
+                stars={item.rating}
                 price={item.price}
                 img={item.image}
             />
