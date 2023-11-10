20e51c6aa751ef2dc6bbe203e90b8366a2b9ac9f
rlnB2022 <beyer.richard73@gmail.com>
2023-11-10 10:32:41 -0600

    Fixed API key error. Added email and password checking, messaging when logging in or creating a new account to store using the Firebase Auth system.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Login from './components/Login';
-import Payment from './components/Payment';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Checkout from './components/Checkout';
-import { useEffect } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import './firebase';
-import { useDispatch } from 'react-redux';
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Payment from "./components/Payment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import "./firebase";
+import { useDispatch } from "react-redux";
 
 function App() {
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+	useEffect(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// user logged in
+				dispatch({
+					type: "SET_USER",
+					user: user,
+				});
+			} else {
+				// user logged out
+				dispatch({
+					type: "SET_USER",
+					user: null,
+				});
+			}
+		});
+	});
 
-  useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, user => {
-      if(user) {
-        // user logged in
-        dispatch({
-          type: "SET_USER",
-          user: user
-        });
-    }
-      else {
-        // user logged out
-        dispatch({
-          type: "SET_USER",
-          user: null
-        });
-      }
-    })
-  }, );
-
-  return (
-    <Router>
-      <div className='App'>
-        <Switch>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/checkout'>
-            <Header />
-            <Checkout />
-          </Route>
-          <Route path='/payment'>
-            <Header />
-            <Payment />
-          </Route>
-          <Route path='/'>
-            <Header />
-            <Main />
-          </Route>
-        </Switch>
-      </div>
-    </Router >
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/checkout">
+						<Header />
+						<Checkout />
+					</Route>
+					<Route path="/payment">
+						<Header />
+						<Payment />
+					</Route>
+					<Route path="/">
+						<Header />
+						<Main />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,78 +1,152 @@
-import './Login.css';
-import logo from '../assets/amazon-logo-white.jpg';
-import { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import '../firebase';
-import { useDispatch } from 'react-redux';
+import "./Login.css";
+import logo from "../assets/amazon-logo-white.jpg";
+import { useState, useEffect, useRef } from "react";
+import { Link, useHistory } from "react-router-dom";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
+import "../firebase";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [emailMessage, setEmailMessage] = useState("");
+	const [passwordMessage, setPasswordMessage] = useState("");
+	const passwordRef = useRef();
 
-    const history = useHistory();
+	const history = useHistory();
 
-    const signin = e => {
-        e.preventDefault();
+	const signin = (e) => {
+		e.preventDefault();
 
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-            .then(userCredential => {
-                //signed in
-                if(auth) {
-                    history.push('/');
-                }
-            })
-            .catch(error => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-            })
-    };
+		const auth = getAuth();
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				//signed in
+				if (auth) {
+					history.push("/");
+				}
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				console.log(errorCode);
+				errorMessageHandler(errorCode);
+			});
+	};
 
-    const createAccount = e => {
-        e.preventDefault();
+	const errorMessageHandler = (errorCode) => {
+		if (errorCode === "auth/invalid-email") {
+			setEmailMessage("Please enter a valid email.");
+		}
+		if (errorCode === "auth/missing-password") {
+			setPasswordMessage("Please enter a valid password.");
+		}
+		if (errorCode === "auth/weak-password") {
+			setPasswordMessage("Password should be at least 6 characters.");
+		}
+		if (errorCode === "auth/email-already-in-use") {
+			setEmailMessage("Email already in use.");
+		}
+		if (errorCode === "auth/wrong-password") {
+			setPasswordMessage("Wrong password entered.");
+		}
+	};
 
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                if(auth) {
-                    history.push('/');
-                }
-                // const user = userCredential.user;
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-            });
+	const createAccount = (e) => {
+		e.preventDefault();
 
-    };
+		const auth = getAuth();
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				if (auth) {
+					history.push("/");
+				}
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				errorMessageHandler(errorCode);
+			});
+	};
 
-    useEffect(() => {
-        const auth = getAuth();
-        auth.signOut();
-    }, []);
+	const showPasswordHandler = (e) => {
+		passwordRef.current.type = e.target.checked ? "text" : "password";
+	};
 
-    return (
-        <div className='login'>
-            <Link to="/">
-                <img src={logo} alt="Amazon Logo" />
-            </Link>
-            <div className='login-container'>
-                <form>
-                    <h1>Sign-In</h1>
-                    <p>Email</p>
-                    <input id='login-email' className='login-input' type='text' name='email' value={email} onChange={e => setEmail(e.target.value)} />
-                    <p>Password</p>
-                    <input id='login-password' className='login-input' type='text' value={password} onChange={e => setPassword(e.target.value)} />
-                    <button id='login-btn' className='login-button' onClick={signin}>Sign In</button>
-                </form>
-                <p>By signing-in, you agree with my Amazon Clone Conditions of Use and Privacy Notice.</p>
-                <button className='login-button login-createaccount' onClick={createAccount}>Create an Amazon Account</button>
-            </div>
-        </div>
-    )
+	useEffect(() => {
+		const auth = getAuth();
+		auth.signOut();
+	}, []);
+
+	return (
+		<div className="login">
+			<Link to="/">
+				<img src={logo} alt="Amazon Logo" />
+			</Link>
+			<div className="login-container">
+				<form>
+					<h1>Sign-In</h1>
+					<p>Email</p>
+					<input
+						id="login-email"
+						className="login-input"
+						type="text"
+						name="email"
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value);
+							setEmailMessage("");
+						}}
+					/>
+					{emailMessage.length > 0 ? (
+						<p className="error-message">{emailMessage}</p>
+					) : (
+						""
+					)}
+					<p>Password</p>
+					<input
+						ref={passwordRef}
+						id="login-password"
+						className="login-input"
+						type="password"
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value);
+							if (e.target.value.length > 5) {
+								setPasswordMessage("");
+							}
+						}}
+					/>
+					{passwordMessage.length > 0 ? (
+						<p className="error-message">{passwordMessage}</p>
+					) : (
+						""
+					)}
+					<input
+						type="checkbox"
+						id="show-password"
+						onChange={showPasswordHandler}
+					/>
+					<label htmlFor="show-password"> Show Password</label>
+					<button id="login-btn" className="login-button" onClick={signin}>
+						Sign In
+					</button>
+				</form>
+				<p>
+					By signing-in, you agree with my Amazon Clone Conditions of Use and
+					Privacy Notice.
+				</p>
+				<button
+					className="login-button login-createaccount"
+					onClick={createAccount}
+				>
+					Create an Amazon Account
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
